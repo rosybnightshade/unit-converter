7ff6eb136772a5418b8e2e9c803a5e7594a6890a
rosybnightshade <oliviaschuetz11@gmail.com>
2025-01-30 16:27:13 -0700

    yay

---
+++ calc.js
@@ -0,0 +1,54 @@
+function convertToMeters(value, fromUnit) {
+    if (fromUnit === "kilometers") {
+        return value * 1000;
+    }
+
+    if (fromUnit === "centimeters") {
+        return value / 100;
+    }
+
+    if (fromUnit === "feet") {
+        return value / 3.28084;
+    }
+
+    if (fromUnit === "inches") {
+        return value / 39.3701;
+    }
+};
+
+function convertFromMeters(meters, toUnit) {
+    if (toUnit === "kilometers") {
+        return meters / 1000;
+    }
+
+    if (toUnit === "centimeters") {
+        return meters * 100;
+    }
+
+    if (toUnit === "feet") {
+        return meters * 3.28084;
+    }
+
+    if (toUnit === "inches") {
+        return meters * 39.3701;
+    }
+    return meters;
+}
+
+function convert() {
+    let input_number = document.getElementById('inputValue').value;
+    console.log(input_number);
+
+    let from_unit = document.getElementById('fromUnit').value;
+    console.log(from_unit);
+
+    let to_unit = document.getElementById('toUnit').value;
+    console.log(to_unit);
+
+    // input to meters
+    const valueInMeters = convertToMeters(Number(input_number), from_unit);
+    // convert result into targeted unit
+    const resultInMeters = convertFromMeters(valueInMeters, to_unit);
+    //display result
+    document.getElementById('result').innerHTML = `${input_number}  ${from_unit} = ${resultInMeters} ${to_unit}`
+}
